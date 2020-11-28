c7d9c97faa7dfd6a75eee8dc5509f2cd8be0367f
kerveros12v <kerveros12v@aol.com>
2020-11-28 17:24:50 -0500

    Version 2 aregando framework para la parte de las vistas

--- web/index.php
+++ web/index.php
@@ -8,7 +8,36 @@
 </head>
 
 <body>
+    <table width="100%" border="0" cellspacing="0">
 
+        <tr>
+
+            <td><img src="img/EsCayambe.png" name="logoindex" width="600" height="250" id="logoindex" /></td>
+
+        </tr>
+
+        <tr>
+
+            <td>
+                <table width="100%" border="0" cellspacing="0">
+                    <tr>
+
+                        <td class="m1login"><a href="Templates/opcionesMatriculacion.php"><img src="img/matricula_1.png" width="419" height="185" />Matriculacion</a></td>
+
+                        <td class="m1login"><a href="Templates/opcionesSetecAspirante.php"><img src="img/logo2018setec.jpg" width="419" height="185" />Setec</a></td>
+
+                        <td><a href="Templates/login.php">Acceso sistema</a></td>
+
+                    </tr>
+
+                </table>
+            </td>
+
+        </tr>
+
+
+
+    </table>
 </body>
 
 </html>
